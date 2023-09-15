07697c1caaed6120f50c22c1148b530e708d7c4c
datttrian <datttrian@gmail.com>
2023-09-15 07:31:42 +0000

    Implementing a linked list

---
+++ src/03_02/03_02-linkedlist1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 16
+
+// Read standard input, discard newline
+char *input(void)
+{
+	static char buffer[SIZE];
+	char *r;
+	int x;
+
+	// Initiate the buffer
+	buffer[0] = '\0';
+
+	// Gather input
+	r = fgets(buffer, SIZE, stdin);
+	if (r == NULL)
+	{
+		fprintf(stderr, "Input error\n");
+		exit(1);
+	}
+
+	// Remove the newline
+	for (x = 0; x < SIZE; x++)
+	{
+		/* remove newline */
+		if (buffer[x] == '\n')
+		{
+			buffer[x] = '\0';
+			break;
+		}
+	}
+
+	return (buffer);
+}
+
+int main()
+{
+	struct item
+	{
+		int id;
+		char name[SIZE];
+		float price;
+		struct item *next;
+	};
+	struct item *first, *current;
+	char temp[SIZE];
+
+	// Allocate initial structure
+	first = (struct item *)malloc(sizeof(struct item) * 1);
+	if (first == NULL)
+	{
+		fprintf(stderr, "Unable to allocate memory\n");
+		exit(1);
+	}
+
+	// The first structure is now the current structure
+	current = first;
+
+	// Fill structure
+	current->id = 1;
+	printf("Record %d Fruit: ", current->id);
+	strcpy(current->name, input());
+	printf("Price: ");
+	strcpy(temp, input());
+	current->price = strtof(temp, NULL);
+
+	// Don't forget to initialize the pointer!
+	current->next = NULL;
+
+	// Output the contents
+	printf("%d: %s for %.2f/pound\n", current->id, current->name, current->price);
+
+	return (0);
+}
---
+++ src/03_02/03_02-linkedlist2.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 16
+
+/* define structure externally to ensure
+   that it's available to all functions */
+struct item
+{
+	int id;
+	char name[SIZE];
+	float price;
+	struct item *next;
+};
+
+// Read standard input, discard newline
+char *input(void)
+{
+	static char buffer[SIZE];
+	char *r;
+	int x;
+
+	// Initiate the buffer
+	buffer[0] = '\0';
+
+	// Gather input
+	r = fgets(buffer, SIZE, stdin);
+	if (r == NULL)
+	{
+		fprintf(stderr, "Input error\n");
+		exit(1);
+	}
+
+	// Remove the newline
+	for (x = 0; x < SIZE; x++)
+	{
+		/* remove newline */
+		if (buffer[x] == '\n')
+		{
+			buffer[x] = '\0';
+			break;
+		}
+	}
+
+	return (buffer);
+}
+
+// Allocate a new structure
+struct item *allocate(void)
+{
+	struct item *p;
+
+	p = (struct item *)malloc(sizeof(struct item) * 1);
+	if (p == NULL)
+	{
+		fprintf(stderr, "Unable to allocate memory\n");
+		exit(1);
+	}
+
+	return (p);
+}
+
+// Fill the structure
+void fill(struct item *s, int i)
+{
+	char temp[SIZE];
+
+	s->id = i;
+	printf("Record %d Fruit: ", i);
+	strcpy(s->name, input());
+	printf("Price: ");
+	strcpy(temp, input());
+	s->price = strtof(temp, NULL); // the 'next' member is filled in the main() function
+}
+
+// Output the list
+void output(struct item *s)
+{
+	while (s != NULL)
+	{
+		printf("%d: %s for %.2f/pound\n", s->id, s->name, s->price);
+		s = s->next;
+	}
+}
+
+int main()
+{
+	struct item *first, *current;
+
+	// Allocate initial structure
+	first = allocate();
+
+	// The base structure is now the current structure
+	current = first;
+
+	// Fill structure
+	fill(current, 1);
+
+	// Cap the final structure
+	current->next = NULL;
+
+	// Output the contents
+	output(first);
+
+	return (0);
+}
